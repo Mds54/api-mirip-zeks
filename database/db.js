9faa41a82c9abf18c5dcb5676cc9b9cd6a4484a4
M5R4 <xzyytr@gmail.com>
2024-12-18 14:11:33 +0800

    db.js

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://kuhong-api.herokuapp.com';
+var url = 'https://api-xzyymds-1.vercel.app';
 try {
-if(url == 'https://kuhong-api.herokuapp.com') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'https://api-xzyymds-1.vercel.app') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
