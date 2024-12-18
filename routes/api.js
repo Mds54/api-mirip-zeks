430b3439131ff2dc2eabad4ef3f8befa7089340e
M5R4 <xzyytr@gmail.com>
2024-12-18 12:18:38 +0800

    api.js

--- routes/api.js
+++ routes/api.js
@@ -3112,6 +3112,24 @@ router.get('/other/kbbi', async (req, res, next) => {
          .catch(e => {
          	console.log(e);
          	res.json(loghandler.error)
+	 })
+		router.get('/other/openai', async (req, res, next) => {
+          var apikey = req.query.apikey
+       	var text = req.query.kata
+       	if(!apikey) return res.json(loghandler.apikey)
+       if(!text) return res.json({ status : false, creator : `${creator}`, message : "masukan parameter kata"})
+        if(listkey.includes(apikey)){
+       fetch(encodeURI(`https://btch.us.kg/openai?text=${text}`))
+        .then(response => response.json())
+        .then(data => {
+        var result = data;
+             res.json({
+                 result
+             })
+         })
+         .catch(e => {
+         	console.log(e);
+         	res.json(loghandler.error)
 })
 } else {
   res.json(loghandler.apikey)
